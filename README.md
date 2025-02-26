5e39f2ff30e1f315b1a042833609659374db9a43
KimYoungWoo <rladuddn3924@gmail.com>
2025-02-27 00:23:25 +0900

    🚀 Firebase 호스팅 업로드

---
+++ README.md
@@ -0,0 +1,3 @@
+# 리액트 프로젝트 성능 개선
+
+url: https://ywkim-hp-front-4th-10-2.web.app/
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./config.ts";
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
